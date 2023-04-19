bd3f510f4d577d5fb6d1200d661838bcbfc917d9
hritsh <hritish.mahajan@gmail.com>
2023-04-19 22:29:09 +0400

    Fix Script 03 Bugs and Format

--- script03/shortcut.py
+++ script03/shortcut.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """
 Name: Hritish Mahajan
-Date: 18 February 2023
+Date: 19 April 2023
 """
 
 import os
-import sys
 import subprocess
 from time import sleep
 
@@ -14,22 +13,18 @@ def clear_screen():
     os.system('clear' if os.name == 'posix' else 'cls')
 
 
-def get_cwd():
-    return os.getcwd()
-
-
 def check_file_exists(file_path):
     return os.path.exists(file_path)
 
 
-def create_shortcut(file_path):
+def create_shortcut_file(file_path):
     shortcut_name = os.path.basename(file_path)
     home_dir = os.path.expanduser('~')
     shortcut_path = os.path.join(home_dir, shortcut_name)
     subprocess.run(['ln', '-s', file_path, shortcut_path])
 
 
-def remove_shortcut(shortcut_name):
+def remove_shortcut_file(shortcut_name):
     home_dir = os.path.expanduser('~')
     shortcut_path = os.path.join(home_dir, shortcut_name)
     if os.path.islink(shortcut_path):
@@ -38,74 +33,118 @@ def remove_shortcut(shortcut_name):
     return False
 
 
+def create_shortcut():
+    clear_screen()
+    file_path = input(
+        "Please enter the file name to create a shortcut: ")
+    if not check_file_exists(file_path):
+        print(f"\nSorry, couldn't find {colorize(file_path, 'red')}!")
+        input("\nPress " + colorize("Enter") +
+              " to return to the main menu.")
+
+    else:
+        confirm = input(
+            f"Found {colorize(file_path)}. Select Y/y to create shortcut. ")
+        if confirm.lower() == "y":
+            create_shortcut_file(file_path)
+            print("Creating Shortcut, please wait.")
+            sleep(2)
+            print()
+            print_shortcut_report()
+            return
+        else:
+            input("Press " + colorize("Enter") +
+                  " to return to the main menu.")
+
+
+def remove_shortcut():
+    clear_screen()
+    shortcut_name = input("Please enter the shortcut/link to remove: ")
+    home_dir = os.path.expanduser('~')
+    shortcut_path = os.path.join(home_dir, shortcut_name)
+    if os.path.islink(shortcut_path) is False:
+        print(f"\nSorry, couldn't find {colorize(shortcut_name, 'red')}!")
+        input("\nPress " + colorize("Enter") +
+              " to return to the main menu.")
+        return
+
+    confirm = input(
+        f"Are you sure you want remove {colorize(shortcut_name)}? Press {colorize('Y/y')} to confirm: ")
+    if confirm.lower() == "y":
+        if remove_shortcut_file(shortcut_name):
+            print("\nRemoving link, please wait...")
+            sleep(2)
+        else:
+            print(f"\nSorry, couldn't find {colorize(shortcut_name, 'red')}!")
+            sleep(2)
+
+    input("\nPress " + colorize("Enter") +
+          " to return to the main menu.")
+
+
 def print_shortcut_report():
     home_dir = os.path.expanduser('~')
     os.chdir(home_dir)
 
-    print("********************")
-    print("** Shortcut Report")
-    print("*****************")
+    print(f"""**************************************
+********** {colorize("Shortcut Report")} ***********
+**************************************""")
     print()
-    print(f"You current directory is {get_cwd()}.")
+    print(f"You current directory is {colorize(cwd, 'yellow')}.")
     links = [entry for entry in os.scandir() if entry.is_symlink()]
-    print(f"The number of links is {len(links)}.")
-    print("Symbolic Link | Target Path")
+    print(f"\nThe number of links is {colorize(str(len(links)), 'yellow')}.\n")
+    print(colorize("Symbolic Link", "underline"),
+          colorize("Target", "underline"), sep="\t\t")
     for link in links:
         target = os.readlink(link.path)
-        print(f"{link.name} | {target}")
+        print(link.name, target, sep="\t\t")
+    choice = input(
+        f"\n\nTo return to the {colorize('Main Menu', 'yellow')}, press {colorize('Enter', 'yellow')}. Or select {colorize('R/r', 'yellow')} to remove a link. ")
+    if choice == "r" or choice == "R":
+        remove_shortcut()
+
+
+def colorize(text, color="green"):
+    color = {'green': '\033[92m', 'red': '\033[91m',
+             'yellow': '\033[93m', 'underline': '\033[4m\033[33m'}[color]
+    return color + text + '\033[0m'
 
 
 def main_menu():
-    clear_screen()
-    print("# Main Menu\n")
-    print("****************")
-    print("********* Shortcut Creator")
-    print("****************\n")
-    print("Enter Selection:")
-    print("1 - Create a shortcut in your home directory.")
-    print("2 - Remove a shortcut from your home directory.")
-    print("3 - Run shortcut report.")
-    print("Please enter a number (1-3) or '/q' to quit the program.")
-    choice = input()
-
-    if choice == "1":
+    global cwd
+    cwd = os.getcwd()
+    while True:
         clear_screen()
-        file_path = input("Please enter the file name to create a shortcut: ")
-        if not check_file_exists(file_path):
-            print("File not found.")
+        print(f"""**************************************
+********** {colorize("Shortcut Creator")} **********
+**************************************
+
+Enter Selection:
+
+1 - Create a shortcut in your home directory.
+2 - Remove a shortcut from your home directory.
+3 - Run shortcut report.
+""")
+        choice = input("Please enter a " + colorize("number (1-4)") +
+                       " or " + colorize('"Q/q"') + " to quit the program: ")
+        if choice == "1":
+            create_shortcut()
+        elif choice == "2":
+            remove_shortcut()
+        elif choice == "3":
+            clear_screen()
+            print_shortcut_report()
+        elif choice == "q" or choice == "Q" or choice == "quit":
+            clear_screen()
+            print("Quitting program: Returning to shell.\n\n")
+            print(colorize("Have a wonderful day!", 'yellow'))
+            input()
+            break
         else:
-            confirm = input(
-                f"Found {file_path}. Select Y/y to create shortcut. ")
-            if confirm.lower() == "y":
-                create_shortcut(file_path)
-                print("Creating Shortcut, please wait.")
-                sleep(2)
-                print_shortcut_report()
-    elif choice == "2":
-        clear_screen()
-        shortcut_name = input("Please enter the shortcut/link to remove: ")
-        confirm = input(
-            f"Are you sure you want remove {shortcut_name}? Press Y/y to confirm: ")
-        if confirm.lower() == "y":
-            if remove_shortcut(shortcut_name):
-                print("Removing link, please wait...")
-                sleep(2)
-                clear_screen()
-            else:
-                print(f"Sorry, couldn't find {shortcut_name}!")
-                sleep(2)
-    elif choice == "3":
-        clear_screen()
-        print_shortcut_report()
-    elif choice == "/q":
-        sys.exit(0)
-    else:
-        print("Invalid choice. Please try again.")
-        sleep(2)
-
-    input("\nTo return to the Main Menu, press Enter.")
-    main_menu()
+            print("Invalid choice. Please try again.")
+            sleep(2)
 
 
 if __name__ == "__main__":
+
     main_menu()
