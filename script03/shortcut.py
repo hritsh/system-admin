28c15bc5f26890581e239f8a94ec504a52df2a10
hritsh <hritish.mahajan@gmail.com>
2023-04-19 22:37:41 +0400

    Document and Complete Script 03

--- script03/shortcut.py
+++ script03/shortcut.py
@@ -10,23 +10,32 @@ from time import sleep
 
 
 def clear_screen():
+    """Clear the screen"""
     os.system('clear' if os.name == 'posix' else 'cls')
 
 
 def check_file_exists(file_path):
+    """Check if file exists"""
     return os.path.exists(file_path)
 
 
 def create_shortcut_file(file_path):
+    """Create a shortcut file in the home directory"""
     shortcut_name = os.path.basename(file_path)
     home_dir = os.path.expanduser('~')
     shortcut_path = os.path.join(home_dir, shortcut_name)
-    subprocess.run(['ln', '-s', file_path, shortcut_path])
+    # check if shortcut already exists
+    if os.path.islink(shortcut_path) is False:
+        os.symlink(file_path, shortcut_path)
+        return True
+    return False
 
 
 def remove_shortcut_file(shortcut_name):
+    """Remove the shortcut file from the home directory"""
     home_dir = os.path.expanduser('~')
     shortcut_path = os.path.join(home_dir, shortcut_name)
+    # check if shortcut exists
     if os.path.islink(shortcut_path):
         os.unlink(shortcut_path)
         return True
@@ -34,9 +43,11 @@ def remove_shortcut_file(shortcut_name):
 
 
 def create_shortcut():
+    """Menu for creating a shortcut file in the home directory"""
     clear_screen()
     file_path = input(
         "Please enter the file name to create a shortcut: ")
+    # check if file exists
     if not check_file_exists(file_path):
         print(f"\nSorry, couldn't find {colorize(file_path, 'red')}!")
         input("\nPress " + colorize("Enter") +
@@ -46,22 +57,27 @@ def create_shortcut():
         confirm = input(
             f"Found {colorize(file_path)}. Select Y/y to create shortcut. ")
         if confirm.lower() == "y":
-            create_shortcut_file(file_path)
-            print("Creating Shortcut, please wait.")
-            sleep(2)
-            print()
-            print_shortcut_report()
-            return
+            if create_shortcut_file(file_path):
+                print("Creating Shortcut, please wait.")
+                sleep(2)
+                print()
+                print_shortcut_report()
+                return
+            else:
+                print(f"\nLink {colorize(file_path, 'red')} already exists!\n")
+
         else:
             input("Press " + colorize("Enter") +
                   " to return to the main menu.")
 
 
 def remove_shortcut():
+    """Menu for removing a shortcut file from the home directory"""
     clear_screen()
     shortcut_name = input("Please enter the shortcut/link to remove: ")
     home_dir = os.path.expanduser('~')
     shortcut_path = os.path.join(home_dir, shortcut_name)
+    # check if shortcut exists
     if os.path.islink(shortcut_path) is False:
         print(f"\nSorry, couldn't find {colorize(shortcut_name, 'red')}!")
         input("\nPress " + colorize("Enter") +
@@ -83,6 +99,7 @@ def remove_shortcut():
 
 
 def print_shortcut_report():
+    """Print a report of all the shortcut files in the home directory"""
     home_dir = os.path.expanduser('~')
     os.chdir(home_dir)
 
@@ -95,6 +112,7 @@ def print_shortcut_report():
     print(f"\nThe number of links is {colorize(str(len(links)), 'yellow')}.\n")
     print(colorize("Symbolic Link", "underline"),
           colorize("Target", "underline"), sep="\t\t")
+    # print links by scanning the home directory and printing the name and target of only the links
     for link in links:
         target = os.readlink(link.path)
         print(link.name, target, sep="\t\t")
@@ -105,6 +123,7 @@ def print_shortcut_report():
 
 
 def colorize(text, color="green"):
+    """Colorize text"""
     color = {'green': '\033[92m', 'red': '\033[91m',
              'yellow': '\033[93m', 'underline': '\033[4m\033[33m'}[color]
     return color + text + '\033[0m'
